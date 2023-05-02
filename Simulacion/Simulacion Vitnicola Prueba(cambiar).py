7eb7b6d213a04dd5fb5d83234ead7103eb4feb65
Mamoreno4 <mamoreno4@uc.cl>
2023-05-02 15:24:21 -0400

    Poblar cuarteles

--- Simulacion/Simulacion Vitnicola Prueba(cambiar).py
+++ Simulacion/Simulacion Vitnicola Prueba(cambiar).py
@@ -4,11 +4,10 @@ import pandas as pd
 from scipy.stats import binom
 
 class Cuartel:
-    def __init__(self, listav,cosecha_aproximada):
-        self.id=listav[0]
-        self.variedad=listav[4]
-        self.precio=listav[5]
-        self.cantidad_dia=cosecha_aproximada
+    def __init__(self, listav):
+        self.id=listav[1]
+        self.variedad=listav[5]
+        self.precio=listav[6]
     def __str__(self):
         return "-> {}".format(self.id)
 class Bodega:
@@ -64,7 +63,7 @@ class Tanque:
             return "Error: tanque no Disponible"
     def vaciar_tanque(self):
         dias_fermentando=self.dia_termino-self.dia_inicial
-        fermentado=[self.cantidad_fermentado,self.variedad_fermentando,dias_fermentando]
+        fermentado=[self.cantidad_fermentado,self.variedad_fermentando,self.variedad_fermentando,dias_fermentando]
         self.cantidad_fermentado=0
         self.variedad_fermentando=""
         self.dia_inicial=0
@@ -77,17 +76,37 @@ class Tanque:
         p=Dist.iloc[0][3]
         dia_generado=binom.rvs(n, p)
         return dia_generado
-
+#Revisar que mas poner en resumen
 class resumen:
     def __init__(self):
         self.dias=dict()
-        for i in range(1:100):
-        
+        for i in range(100):
+            self.dias["Dia "+i]=[]
         pass
+    def agregar_dia(self,dia):
+        self.dias["Dia "+dia]=[]
+        pass
+    def agregar_cosecha(self,dia,cosecha):
+        self.dias["Dia "+dia].append("Se cosecho "+cosecha)
+        pass
+    def agregar_fermentado(self,dia,fermentado):
+        self.dias["Dia "+dia].append("Se fermento "+fermentado[0]+" de variedad "+fermentado[1]+" de precio "+fermentado[2]+" en "+ fermentado[3] +" dias")
+        pass
+    def agregar_sobrante(self,dia,sobrante):
+        self.dias["Dia "+dia].append("El dia"+ dia +" sobro la cantidad de "+sobrante)
+        pass
+    
 
 
 
 Distribuciones= pd.read_excel('./../Distribuciones/dist.xlsx')
+
+
+Cuarteles= pd.read_excel('Datos Base Ordenados (Cosecha).xlsx')
+Los_Cuarteles=[]
+for i in range(60):
+    CT=Cuartel(Cuarteles.iloc[i])
+    Los_Cuarteles.append(CT)
 
 
 
@@ -107,7 +126,7 @@ Distribuciones= pd.read_excel('./../Distribuciones/dist.xlsx')
         #generar dia(listo)
         #revisar tanques(llamar vaciar tanques)(listo)
     #Resumen
-        #crear estructura
+        #crear estructura(casi listo)
 
 #Leer datos
     #Leer excel
