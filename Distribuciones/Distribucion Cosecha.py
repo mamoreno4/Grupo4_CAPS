48336b8fa0a1b5db75de7d72dc56707c787754c1
Mamoreno4 <mamoreno4@uc.cl>
2023-05-03 12:42:58 -0400

    distribucion cosecha

--- Distribuciones/Distribucion Cosecha.py
+++ Distribuciones/Distribucion Cosecha.py
@@ -9,11 +9,10 @@ import math
 #Leer excel
 #Iniciar  distr
 #Cosechas
-df = pd.read_excel('Datos.xlsx', sheet_name='Hoja1')
-
+df = pd.read_excel("Datos base G4 (2).xlsx", sheet_name='Histórico de cosechas')
 x = np.array(df)
 # Fit distributions
-dist = distfit(method='discrete')
+dist = distfit()
 dist.fit_transform(x)
 
 # Print the results
@@ -21,3 +20,8 @@ print(dist.summary)
 # Plot the fitted distributions
 dist.plot()
 print(dist.plot_summary())  
+a=dist.model["params"][0]
+b=dist.model["params"][1]
+c=dist.model["params"][2]
+d=dist.model["params"][3]
+Generar_data_cuarteles=sc.stats.beta.rvs(a,b,loc=c,scale=d,size=60)
