2e756286f3115f624aafe5186dc389ad9a498c78
M I <martin@Martins-MacBook-Air.local>
2023-05-02 20:47:51 -0400

    Falta rellenar tanques

--- Simulacion/clases_simulacion.py
+++ Simulacion/clases_simulacion.py
@@ -3,14 +3,20 @@ import numpy as np
 import pandas as pd
 from scipy.stats import binom
 
+
 class Cuartel:
+
     def __init__(self, listav):
         self.id=listav[1]
         self.variedad=listav[5]
         self.precio=listav[6]
+
     def __str__(self):
         return "-> {}".format(self.id)
+
+
 class Bodega:
+
     def __init__(self,ubicacion_tanques):
         self.ubicacion=ubicacion_tanques[0]
         self.tanques=[]
@@ -32,17 +38,13 @@ class Bodega:
         for i in range(tanques_100):
             T=Tanque(100)
             self.agregar_tanque(T)
-        
- 
-        
-            
-
-
         pass
+
     def agregar_tanque(self,tanque):
         self.tanques.append(tanque)
         self.tanques_disponibles.append(tanque)
         pass
+
     def revisar_tanques(self,dia):
         salidas=[]
         self.tanques_disponibles=[]
@@ -57,13 +59,16 @@ class Bodega:
                 else:
                     self.tanques_fermentando.append(i)
         return salidas
+    
     def tanques_disponibles(self):
         disp=[]
         for i in self.tanques_disponibles:
             disp.append([i,i.capacidad])
         return disp
 
+
 class Tanque:
+
     def __init__(self,capacidad):
         self.capacidad=capacidad
         self.estado="Disponible"
@@ -73,7 +78,8 @@ class Tanque:
         self.variedad_fermentando=""
         self.precio=0
         pass
-    def fermentar(self,cantidad,dia,variedad,precio,distr):
+
+    def fermentar(self, cantidad, dia, variedad, precio, distr):
         if self.estado=="Disponible":
             self.estado="Fermentando"
             self.dia_inicial=dia
@@ -84,6 +90,7 @@ class Tanque:
             return "Fermentando"
         else:
             return "Error: tanque no Disponible"
+        
     def vaciar_tanque(self):
         dias_fermentando=self.dia_termino-self.dia_inicial
         fermentado=[self.cantidad_fermentado,self.variedad_fermentando,self.variedad_fermentando,dias_fermentando]
@@ -93,28 +100,36 @@ class Tanque:
         self.dia_termino=0
         self.estado="Disponible"
         return fermentado
+    
     def generar_dia(self,dia,variedad,distr):
         Dist=distr[distr["Cepa"]==variedad]
         n=Dist.iloc[0][2]
         p=Dist.iloc[0][3]
         dia_generado=binom.rvs(n, p)
         return dia_generado
+    
+
 #Revisar que mas poner en resumen
-class resumen:
+class Resumen:
+
     def __init__(self):
         self.dias=dict()
         for i in range(100):
             self.dias["Dia "+i]=[]
         pass
+
     def agregar_dia(self,dia):
         self.dias["Dia "+dia]=[]
         pass
+
     def agregar_cosecha(self,dia,cosecha):
         self.dias["Dia "+dia].append("Se cosecho "+cosecha)
         pass
+
     def agregar_fermentado(self,dia,fermentado):
         self.dias["Dia "+dia].append("Se fermento "+fermentado[0]+" de variedad "+fermentado[1]+" de precio "+fermentado[2]+" en "+ fermentado[3] +" dias")
         pass
+
     def agregar_sobrante(self,dia,sobrante):
         self.dias["Dia "+dia].append("El dia"+ dia +" sobro la cantidad de "+sobrante)
         pass
